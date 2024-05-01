72244b7392fb8c63c8b46f4e434a6e1d3c1370bd
Raja Ramesh <b.rajaramesh123@gmail.com>
2024-05-01 12:20:38 +0530

    first commit

---
+++ orders/models.py
@@ -0,0 +1,24 @@
+from django.db import models
+from vendors.models import Vendor
+
+STATUS = (
+    ('pending', 'PENDING'),
+    ('completed', 'COMPLETED'),
+    ('canceled', 'CANCELED'),
+)
+
+class PurchaseOrder(models.Model):
+    id = models.AutoField(primary_key=True, db_index=True, db_column='id')
+    poNumber = models.CharField(max_length=100, db_column='po_number', unique=True)
+    vendor = models.ForeignKey('Vendor', db_column='vendor_id', null=True, on_delete=models.SET_NULL)
+    orderDate = models.DateTimeField(db_column='order_date')
+    deliveryDate = models.DateTimeField(db_column='delivery_date')
+    items = models.JSONField(db_column='items')
+    quantity = models.IntegerField(db_column='quantity', null=False, default=0)
+    status = models.CharField(max_length=20, choices=STATUS, db_column='status', default='pending')
+    qualityRating = models.FloatField(db_column='quality_rating',null=True, blank=True)
+    issueDate = models.DateTimeField(db_column='issue_date')
+    acknowledgmentDate = models.DateTimeField(db_column='acknowledgment_date',null=True, blank=True)
+
+    def __str__(self):
+        return self.po_number
---
+++ vendors/models.py
@@ -0,0 +1,27 @@
+from django.db import models
+
+class Vendor(models.Model):
+    id = models.AutoField(primary_key=True, db_index=True, db_column='id')
+    vendorCode = models.CharField(max_length=20, unique=True, null=False)
+    name = models.CharField(db_column='name', max_length=1000, null=True)
+    contactDetails = models.TextField(db_column='contact_details', null=True, blank=True)
+    address = models.TextField(db_column='address', null=True, blank=True)
+    onTimeDeliveryRate = models.FloatField(db_column='on_time_delivery_rate', default=0)
+    qualityRatingAvg = models.FloatField(db_column='quality_rating_avg', default=0)
+    averageResponseTime = models.FloatField(db_column='average_response_time', default=0)
+    fulfillmentRate = models.FloatField(db_column='fulfillment_rate', default=0)
+
+    def __str__(self):
+        return self.name
+    
+class VendorPerformance(models.Model):
+    id = models.AutoField(primary_key=True, db_index=True, db_column='id')
+    vendor = models.ForeignKey('Vendor', db_column='vendor_id', null=True, on_delete=models.SET_NULL)
+    date = models.DateTimeField(db_column='created_date')
+    onTimeDeliveryRate = models.FloatField(db_column='on_time_delivery_rate', default=0)
+    qualityRatingAvg = models.FloatField(db_column='quality_rating_avg', default=0)
+    averageResponseTime = models.FloatField(db_column='average_response_time', default=0)
+    fulfillmentRate = models.FloatField(db_column='fulfillment_rate', default=0)
+
+    def __str__(self):
+        return f"{self.vendor} - {self.date}"
